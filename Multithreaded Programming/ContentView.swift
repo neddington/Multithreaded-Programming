2938a2351a314da349a08d7b72afdbbaceab22a8
neddington <neddington@cascadia.edu>
2023-11-06 12:32:28 -0800

    Fin

--- Multithreaded Programming/ContentView.swift
+++ Multithreaded Programming/ContentView.swift
@@ -8,17 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    // State variables to track the app's state
+    @State private var isWorking = false
+    @State private var progress: Double = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Long-Running Work Example")
+                .font(.largeTitle)
+                .padding()
+            
+            // Button to start the long-running work
+            Button(action: {
+                self.startLongRunningTask()
+            }) {
+                Text("Start Long Work")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+            // Slider to show progress
+            Slider(value: $progress, in: 0...1)
+                .padding()
+                .disabled(isWorking) // Disable the slider when work is in progress
         }
-        .padding()
+    }
+    
+    // Function to simulate long-running work
+    func startLongRunningTask() {
+        isWorking = true
+        DispatchQueue.global().async {
+            for i in 1...100 {
+                // Simulate work by sleeping for a short time
+                usleep(100_000) // Sleep for 100ms
+                DispatchQueue.main.async {
+                    // Update the progress on the main thread
+                    self.progress = Double(i) / 100.0
+                }
+            }
+            DispatchQueue.main.async {
+                isWorking = false
+            }
+        }
     }
 }
-
 #Preview {
     ContentView()
 }
