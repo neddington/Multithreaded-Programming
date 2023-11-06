e87d20fefab3798e2307a51c227ab48e893c0219
neddington <neddington@cascadia.edu>
2023-11-06 12:30:15 -0800

    Initial Commit

---
+++ Multithreaded Programming/Multithreaded_ProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multithreaded_ProgrammingApp.swift
+//  Multithreaded Programming
+//
+//  Created by Eddington, Nick on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Multithreaded_ProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
